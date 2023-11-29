09bdc65e8cb312042c77f56c0e55ecd8c78a93e0
Tom Brophy <tombrophy101@outlook.com>
2023-11-29 14:28:31 +0000

    first commit

---
+++ Gallery Page/external.js
@@ -0,0 +1,9 @@
+function game() {
+	var answer = prompt("Do you care about sea life?");
+		
+		if (answer == "yes") {
+			alert("This website is for you.");
+		} else {
+			alert("This website isn't for you.");
+		}
+}
