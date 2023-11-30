c25256111ab03a718cf7be163745f978381095bd
Tom Brophy <tombrophy101@outlook.com>
2023-11-30 16:31:34 +0000

    update files

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,9 +1,13 @@
-function game() {
-	var answer = prompt("Do you care about sea life?");
-		
-		if (answer == "yes") {
-			alert("This website is for you.");
-		} else {
-			alert("This website isn't for you.");
-		}
+function signIn(input) {
+	if (form.userid.value == "admin" && form.pwd.value == "noahgrocery"){
+      form.action = "#";
+      return true;
+  }
+  else if(form.userid.value == "admin2" && form.pwd.value == "noahgrocery2"){
+      form.action = "#";
+      return true;
+  } else {
+    alert("Incorrect Password or Username")
+    return false;
+  }
 }
